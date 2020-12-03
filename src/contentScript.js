5b787409f3a28167f20335eb61cf5489c0af8f85
lecepin <383810086@qq.com>
2020-12-03 13:45:38 +0800

    chore: 调整时机

--- src/contentScript.js
+++ src/contentScript.js
@@ -1,19 +1,18 @@
-chrome.storage.sync.get(
-  { enable: true, timeout: 1000, click_count: 3 },
-  function (config) {
-    const { enable, timeout, click_count } = config;
+window.addEventListener("click", (e) => {
+  chrome.storage.sync.get(
+    { enable: true, timeout: 1000, click_count: 3 },
+    function (config) {
+      const { enable, timeout, click_count } = config;
 
-    enable &&
-      window.addEventListener("click", (e) => {
-        window.__kill_tab_c_count = (window.__kill_tab_c_count || 0) + 1;
-        if (window.__kill_tab_c_count >= click_count) {
-          // window.close();
-          chrome.runtime.sendMessage({ close: true });
-        }
+      window.__kill_tab_c_count = (window.__kill_tab_c_count || 0) + 1;
+      if (window.__kill_tab_c_count >= click_count) {
+        // window.close();
+        chrome.runtime.sendMessage({ close: true });
+      }
 
-        setTimeout(() => {
-          window.__kill_tab_c_count = 0;
-        }, timeout);
-      });
-  }
-);
+      setTimeout(() => {
+        window.__kill_tab_c_count = 0;
+      }, timeout);
+    }
+  );
+});
